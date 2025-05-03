9ef5bf16f989fd5cd82451fb82a0cef6f10c47e4
Pedro Leite <pgleite1990@gmail.com>
2025-05-03 18:05:20 +0100

    small adaptation for landscape screens and author note

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,20 @@ import ProfileCard from "./components/ProfileCard";
 
 const App = () => {
   return (
-    <div className="flex justify-center items-center bg-black h-screen font-inter">
-      <main className="">
+    <div className="flex justify-center items-center bg-black min-h-screen h-full py-10 font-inter relative">
+      <main>
         <ProfileCard />
       </main>
+      <p className="text-xs absolute text-white bottom-0">
+        Challenge by{" "}
+        <a className="font-bold text-futuristic-green" href="https://www.frontendmentor.io/" target="_blank">
+          Frontend Mentor.{" "}
+        </a>{" "}
+        Coded by{" "}
+        <a className="font-bold text-futuristic-green" href="https://github.com/pedrogl1990" target="_blank">
+          Pedro Leite.
+        </a>
+      </p>
     </div>
   );
 };
